254e49f42eab1a7e0adc733836244fbda40c85ba
suzuki <tateos.study.only@gmail.com>
2019-09-09 20:58:44 +0900

    課題

--- rpg/src/javaTest/Rpg.java
+++ rpg/src/javaTest/Rpg.java
@@ -3,8 +3,36 @@ package javaTest;
 public class Rpg {
 
 	public static void main(String[] args) {
-	System.out.println("開始");
 
+//		キャラクターを新規作成
+		Character a1 = new Character();
+		a1.name = "でんきネズミ";
+		a1.hp = 100;
+		a1.mp = 0;
+		a1.attack = 40;
+		a1.defence = 50;
+		a1.speed = 30;
+
+//		キャラクターを新規作成
+		Character b1 = new Character();
+		b1.name = "みずネズミ";
+		b1.hp = 300;
+		b1.mp = 200;
+		b1.attack = 80;
+		b1.defence = 70;
+		b1.speed = 60;
+
+
+//		Characterクラスで作成したメソッドを呼び出す
+		a1.introduce();
+		a1.strength();
+
+//		見やすくするため空行を入れる
+		System.out.println("");
+
+//		Characterクラスで作成したメソッドを呼び出す
+		b1.introduce();
+		b1.strength();
 	}
 
 }
