1383d833c1ef288227aad9baea18098e7e86f534
suzuki <tateos.study.only@gmail.com>
2019-09-09 20:59:24 +0900

    キャラクター作成課題

--- rpg/src/javaTest/Character.java
+++ rpg/src/javaTest/Character.java
@@ -2,4 +2,20 @@ package javaTest;
 
 public class Character {
 
+	public String name;
+	public int hp;
+	public int mp;
+	public int attack;
+	public int defence;
+	public int speed;
+
+
+//	メソッド作成
+	public void introduce() {
+		System.out.println("僕の名前は" + name + ",HPは" + hp + ",MPは" + mp + " !！" );
+	}
+
+	public void strength() {
+		System.out.println("攻撃力:" + attack + ",防御力:" + defence + ",素早さ:" + speed);
+	}
 }
